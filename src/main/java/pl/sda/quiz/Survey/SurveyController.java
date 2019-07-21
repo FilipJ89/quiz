e2d9037db20ace40cd368077d82f03862f2f076a
Filip Jozwiakowski <filip-jozwiakowski@wp.pl>
2019-07-21 10:10:13 +0200

    create form work started. Login and registration forms template correction

--- src/main/java/pl/sda/quiz/Survey/SurveyController.java
+++ src/main/java/pl/sda/quiz/Survey/SurveyController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SurveyController {
 
-    // TODO mapping pages with parameters to show all or only surveys or only quizes on the main page using "MainPAgeView" interface. All should be a default option when loading the page
-    @GetMapping("/survey")
-    public String showRegisterForm() {
-        return "index";
+    @GetMapping("/new")
+    public String showSurveyCreationForm() {
+        return "surveyForm";
     }
 }
